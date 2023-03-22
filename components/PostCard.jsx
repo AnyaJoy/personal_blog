7ed288acbe0bef9a21d79ea6334ce481861e16b3
anyajoymedcase <anya.jo@medcase.health>
2023-03-22 11:10:58 +0200

    fixed mobile front page

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 export default function PostCard({ post }) {
   return (
-    <div className="relative bg-white shadow-lg rounded-lg lg:p-5 pb-12 mb-8">
+    <div className="relative bg-white shadow-lg rounded-lg p-5 pb-12 mb-8">
       <Link href={`/post/${post.slug}`}>
         <div className="grid grid-cols-1 lg:grid-cols-6 gap-6">
           <div className="lg:col-span-2 col-span-1">
@@ -12,13 +12,13 @@ export default function PostCard({ post }) {
               <img
                 src={post.featuredImage.url}
                 alt={post.title}
-                className="absolute h-40 shadow-lg rounded-t-lg lg:rounded-lg opacity-20 hover:opacity-100 transition duration-700 overflow:hidden"
+                className="absolute h-40 shadow-lg rounded-t-lg rounded-lg opacity-20 hover:opacity-100 transition duration-700 overflow:hidden"
               />
               <p className="title text-brown">{post.title}</p>
             </div>
           </div>
           <div className="lg:col-span-4 col-span-1 bg-brown-300 ">
-            <p className="text-m leading-6 text-brown h-40 text-ellipsis overflow-hidden">
+            <p className="text-m leading-6 text-brown h-40 pt-3 text-ellipsis overflow-hidden ">
               {post.excert}
             </p>
           </div>
